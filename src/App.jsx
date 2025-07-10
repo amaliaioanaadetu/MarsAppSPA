011a0f56a3394ebc2562df65faa5be7d209b3efe
amaliaioanaadetu <Amalia.Adetu@softwire.com>
2025-07-10 15:00:51 +0300

    finished part2

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,23 +6,50 @@ import NASA from "./components/NASA.jsx";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [clickCount, setClickCount] = useState(getInitialClickCount());
+
+    function getInitialClickCount() {
+        if(localStorage.getItem("clickCount")){
+            return parseInt(localStorage.getItem("clickCount"));
+        }
+        return 0;
+    }
+
+    useEffect(() => {
+        localStorage.setItem("clickCount", clickCount.toString());
+    }, [clickCount]);
+
+    function incrementCount() {
+        setClickCount(clickCount + 1);
+    }
+
+    // const [count, setCount] = useRef(0)
+    // localStorage.setItem('count', 0);
+    // // const [count, setCount] = useState(0)
+    // useEffect(() => {
+    //     count.current += 1;
+    // });
+
+    /*
+    const count = useRef(0)
+    const [, setRerender] = useState(false);
+    function increment() {
+          count.current = count.current + 1;
+          setRerender(r => !r);  //???? why works, why doesn't work with localStorage
+        // const c = parseInt(localStorage.getItem('count'), 10) + 1
+        // localStorage.setItem('count', c.toString())
+        // return c;
+    }
+    */
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
+      <div className="NASA">
           <NASA/>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      </div>
+      <div className="Card">
+        <button onClick={incrementCount}>
+          count is {clickCount}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
